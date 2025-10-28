3e989d609ff6fef13c79b5e61aebedc9ab36420d
Thomas Rohloff <v10lator@myway.de>
2025-10-28 23:09:18 +0100

    Experimental: Use ALHP to speed up build
    Signed-off-by: Thomas Rohloff <v10lator@myway.de>

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM archlinux:base-devel
 
-RUN pacman -Syu  --noconfirm bc cpio pahole python git openssh llvm clang lld
-RUN echo -en "y\ny\n" | pacman -Scc
-RUN echo "MAKEFLAGS=\"-j$(nproc --all)\"" >> /etc/makepkg.conf
+RUN mkdir /docker
+ADD docker /docker/
+RUN docker/setup.sh
 WORKDIR /project
---
+++ docker/setup.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/bash
+
+pushd /docker
+  echo "MAKEFLAGS=\"-j$(nproc --all)\"" >> /etc/makepkg.conf
+
+  pacman-key --init
+  pacman -Syu --noconfirm git openssh
+
+  git clone https://aur.archlinux.org/alhp-keyring.git
+  git clone https://aur.archlinux.org/alhp-mirrorlist.git
+  chown -R nobody:nobody alhp-keyring alhp-mirrorlist
+  pushd alhp-keyring
+    sudo -u nobody makepkg --skippgpcheck
+  popd
+  pushd alhp-mirrorlist
+    sudo -u nobody makepkg --skippgpcheck
+  popd
+  pacman -U --noconfirm alhp-keyring/alhp-keyring-*.pkg.tar.* alhp-mirrorlist/alhp-mirrorlist-*.pkg.tar.*
+
+  cpu_ver=$(/usr/lib/ld-linux-x86-64.so.2 --help | grep searched | grep supported | head -n1 | xargs | cut -d ' ' -f 1)
+  case $cpu_ver in
+    x86-64-v4)
+      mv pacman.conf-x86-64-v4 /etc/pacman.conf
+      ;;
+    x86-64-v3)
+      mv pacman.conf-x86-64-v3 /etc/pacman.conf
+      ;;
+    x86-64-v2)
+      mv pacman.conf-x86-64-v2 /etc/pacman.conf
+      ;;
+    *)
+      mv pacman.conf /etc/pacman.conf
+      ;;
+  esac
+
+  echo -en "y\ny\n" | pacman -Scc
+  pacman -Syu --noconfirm bc cpio pahole python llvm clang lld
+  echo -en "y\ny\n" | pacman -Scc
+popd
+rm -rf /docker /usr/share/man /usr/share/info /usr/share/locale
