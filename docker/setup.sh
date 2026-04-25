008a340942be92355ec5784bf1a3f34fb2df1df6
Thomas Rohloff <v10lator@myway.de>
2026-04-25 11:21:15 +0200

    Docker: Remove ALHP
    Signed-off-by: Thomas Rohloff <v10lator@myway.de>

--- docker/setup.sh
+++ docker/setup.sh
@@ -1,42 +1,12 @@
 #!/usr/bin/bash
 
 pushd /docker
-  pacman-key --init
-  pacman -Syu --noconfirm git openssh
-
-  num_cpus=$(nproc --all)
   echo "MAKEFLAGS=\"-j$(nproc --all)\"" >> makepkg.conf
   mv makepkg.conf /etc/makepkg.conf.d/charcoal.conf
+  mv pacman.conf /etc/pacman.conf
 
-  git clone https://aur.archlinux.org/alhp-keyring.git
-  git clone https://aur.archlinux.org/alhp-mirrorlist.git
-  chown -R nobody:nobody alhp-keyring alhp-mirrorlist
-  pushd alhp-keyring
-    sudo -u nobody makepkg --skippgpcheck
-  popd
-  pushd alhp-mirrorlist
-    sudo -u nobody makepkg --skippgpcheck
-  popd
-  pacman -U --noconfirm alhp-keyring/alhp-keyring-*.pkg.tar.* alhp-mirrorlist/alhp-mirrorlist-*.pkg.tar.*
-
-  cpu_ver=$(/usr/lib/ld-linux-x86-64.so.2 --help | grep searched | grep supported | head -n1 | xargs | cut -d ' ' -f 1)
-  case $cpu_ver in
-    x86-64-v4)
-      mv pacman.conf-x86-64-v4 /etc/pacman.conf
-      ;;
-    x86-64-v3)
-      mv pacman.conf-x86-64-v3 /etc/pacman.conf
-      ;;
-    x86-64-v2)
-      mv pacman.conf-x86-64-v2 /etc/pacman.conf
-      ;;
-    *)
-      mv pacman.conf /etc/pacman.conf
-      ;;
-  esac
-
-  echo -en "y\ny\n" | pacman -Scc
-  pacman -Syu --noconfirm bc cpio pahole python llvm clang lld polly
+  pacman-key --init
+  pacman -Syu --noconfirm bc cpio pahole python llvm clang lld polly git openssh
   echo -en "y\ny\n" | pacman -Scc
 popd
 rm -rf /docker /usr/share/man /usr/share/info /usr/share/locale
